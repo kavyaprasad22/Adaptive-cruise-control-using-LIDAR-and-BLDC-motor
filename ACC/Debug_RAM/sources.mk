e9c7215cf8c6ac96a21183ef36d39e56250c4878
kavyaprasad22 <71159241+kavyaprasad22@users.noreply.github.com>
2020-09-11 23:40:12 -0400

    Add files via upload

---
+++ ACC/Debug_RAM/sources.mk
@@ -0,0 +1,69 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+TODISASSEMBLE_SRCS_QUOTED := 
+OBJ_SRCS_OS_FORMAT := 
+S_UPPER_SRCS_QUOTED := 
+TODISASSEMBLE_SRCS := 
+O_SRCS_QUOTED := 
+LD_SRCS_OS_FORMAT := 
+ASM_UPPER_SRCS := 
+TODISASSEMBLE_SRCS_OS_FORMAT := 
+O_SRCS_OS_FORMAT := 
+ASM_UPPER_SRCS_QUOTED := 
+TOPREPROCESS_SRCS_OS_FORMAT := 
+O_SRCS := 
+TOPREPROCESS_SRCS_QUOTED := 
+S_SRCS_OS_FORMAT := 
+C_SRCS_QUOTED := 
+S_SRCS_QUOTED := 
+ASM_SRCS_OS_FORMAT := 
+C_SRCS_OS_FORMAT := 
+ASM_SRCS := 
+S_UPPER_SRCS_OS_FORMAT := 
+S_UPPER_SRCS := 
+ASM_SRCS_QUOTED := 
+OBJ_SRCS_QUOTED := 
+LD_SRCS := 
+LD_SRCS_QUOTED := 
+OBJ_SRCS := 
+S_SRCS := 
+TOPREPROCESS_SRCS := 
+C_SRCS := 
+ASM_UPPER_SRCS_OS_FORMAT := 
+C_DEPS_OS_FORMAT := 
+OBJS_OS_FORMAT := 
+EXECUTABLES_OS_FORMAT := 
+C_DEPS_QUOTED := 
+EXECUTABLES := 
+OBJS := 
+OBJS_QUOTED := 
+EXECUTABLES_QUOTED := 
+C_DEPS := 
+
+# Every subdirectory with source files must be described here
+SUBDIRS := \
+Sources \
+SDK/rtos/osif \
+SDK/platform/hal/src/smc \
+SDK/platform/hal/src/sim/S32K144 \
+SDK/platform/hal/src/scg \
+SDK/platform/hal/src/port \
+SDK/platform/hal/src/pmc \
+SDK/platform/hal/src/pcc \
+SDK/platform/hal/src/lpuart \
+SDK/platform/hal/src/edma \
+SDK/platform/hal/src/dmamux \
+SDK/platform/drivers/src/pins \
+SDK/platform/drivers/src/lpuart \
+SDK/platform/drivers/src/interrupt \
+SDK/platform/drivers/src/edma \
+SDK/platform/drivers/src/clock \
+SDK/platform/drivers/src/clock/S32K144 \
+SDK/platform/devices \
+SDK/platform/devices/S32K144/startup \
+Project_Settings/Startup_Code \
+Project_Settings/Linker_Files \
+Generated_Code \
+
